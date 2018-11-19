879e96ff08e4d506943764c177a0ae64e4755975
Gustavo Otero <gustavo@otero.io>
2018-11-19 15:57:36 -0500

    Set notifications intervals

--- main.go
+++ main.go
@@ -12,9 +12,11 @@ import (
 )
 
 type Target struct {
-	Host               string `yaml:"host"`
-	ExpectedStatusCode int    `yaml:"expected_status_code"`
-	Failing            bool   `yaml:"-"`
+	Host               string    `yaml:"host"`
+	ExpectedStatusCode int       `yaml:"expected_status_code"`
+	NotificationsCount int       `yaml:"-"`
+	LastNotification   time.Time `yaml:"-"`
+	CurrentStatus      string    `yaml:"-"`
 }
 
 type EmailSender struct {
@@ -31,12 +33,15 @@ type EmailNotification struct {
 }
 
 type Config struct {
-	PingInterval      int64             `yaml:"ping_interval"`
-	Targets           []Target          `yaml:"targets"`
-	EmailNotification EmailNotification `yaml:"email_notification"`
+	PingInterval         int64             `yaml:"ping_interval"`
+	NotificationInterval int64             `yaml:"notification_interval"`
+	Targets              []Target          `yaml:"targets"`
+	EmailNotification    EmailNotification `yaml:"email_notification"`
+	MaxNotifications     int               `yaml:"max_notifications"`
 }
 
 func main() {
+
 	b, err := ioutil.ReadFile("conf.yml")
 	if err != nil {
 		panic(err)
@@ -46,42 +51,58 @@ func main() {
 	if err := yaml.Unmarshal(b, c); err != nil {
 		panic(err)
 	}
-
+	log.Println(c.MaxNotifications)
 	run(c)
 }
 
-var running = true
-
 func run(c *Config) {
-	for running {
-		for _, t := range c.Targets {
+	for true {
+		for i, _ := range c.Targets {
+			// Interval between targets ping
+			t := &c.Targets[i]
 			time.Sleep(time.Second)
-
-			r, err := ping(t.Host)
-			if err != nil {
-				log.Printf("ERROR: Ping failed for target %s due %s\n", t.Host, err)
-				sendNotifications(c.EmailNotification, fmt.Sprintf("ERROR: Ping failed for target %s due %s\n", t.Host, err))
-				continue
-			}
-
-			if r != nil {
-				if r.StatusCode != t.ExpectedStatusCode {
-					log.Printf("ERROR: Status Code (%d) is not expected(%d)\n", r.StatusCode, t.ExpectedStatusCode)
-					sendNotifications(c.EmailNotification, fmt.Sprintf("Got invalid status code from target: %s", t.Host))
-					continue
+			if err := healthCheck(t); err != nil {
+				if time.Now().Unix() > (t.LastNotification.Unix()+(c.NotificationInterval*60)) && t.NotificationsCount < c.MaxNotifications {
+					subject := "Healthcheck fail: " + t.Host
+					if t.CurrentStatus != "" {
+						subject = "REMINDER: " + subject
+					}
+					sendNotifications(c.EmailNotification, subject, err.Error())
+					t.LastNotification = time.Now()
+					t.NotificationsCount++
 				}
+				t.CurrentStatus = err.Error()
 			} else {
-				sendNotifications(c.EmailNotification, fmt.Sprintf("Got null response from target: %s", t.Host))
-				continue
+				if t.CurrentStatus != "" {
+					sendNotifications(c.EmailNotification, fmt.Sprintf("Back to normal: %s", t.Host), fmt.Sprintf("All OK: %s back to normal :)", t.Host))
+					t.NotificationsCount = 0
+					t.CurrentStatus = ""
+				}
 			}
-			log.Printf("OK: Ping target %s success\n", t.Host)
 		}
+		// Interval between pings cicle
 		time.Sleep(time.Second * time.Duration(c.PingInterval))
 	}
 }
 
-func healthCheck() {
+func healthCheck(t *Target) error {
+	r, err := ping(t.Host)
+	if err != nil {
+		log.Printf("ERROR: Ping failed for target %s due %s\n", t.Host, err)
+		return fmt.Errorf("ERROR: Ping failed for target %s due %s", t.Host, err)
+	}
 
+	if r != nil {
+		if r.StatusCode != t.ExpectedStatusCode {
+			log.Printf("ERROR: Status Code (%d) is not expected(%d)\n", r.StatusCode, t.ExpectedStatusCode)
+			return fmt.Errorf("ERROR: Status Code (%d) is not expected(%d)", r.StatusCode, t.ExpectedStatusCode)
+		}
+	} else {
+		log.Printf("Got null response from target: %s\n", t.Host)
+		return fmt.Errorf("Got null response from target: %s", t.Host)
+	}
+	log.Printf("OK: Ping target %s success\n", t.Host)
+	return nil
 }
 
 type Response struct {
@@ -108,11 +129,12 @@ func ping(target string) (*Response, error) {
 	return r, nil
 }
 
-func sendNotifications(en EmailNotification, message string) {
+func sendNotifications(en EmailNotification, status string, message string) {
+
 	for _, to := range en.To {
 		msg := "From: " + en.From.User + "\n" +
 			"To: " + to + "\n" +
-			"Subject: Status Changed: \n\n" +
+			"Subject: " + status + " \n\n" +
 			message
 
 		err := smtp.SendMail(fmt.Sprintf("%s:%d", en.From.SMTPHost, en.From.SMTPPort),
